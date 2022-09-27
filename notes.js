d82ff3b0f9dc0efaac33670d81688dc43fd0849c
Chiemezuo <chiemezuoakujobi@gmail.com>
2022-09-27 14:49:45 +0100

    =Init commit

---
+++ notes.js
@@ -0,0 +1,17 @@
+function addNotes(title, message) {}
+
+function getNotes() {}
+
+function removeNote(title) {}
+
+function getNote(title) {}
+
+function loadNotes() {}
+
+module.exports = {
+  addNotes,
+  getNote,
+  getNotes,
+  removeNote,
+  loadNotes,
+};
